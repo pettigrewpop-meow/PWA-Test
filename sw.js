555681c0bab39c0349ee5d3bdd5b00c49dcebf32
Petti Grew <pettigrew.pop@gmail.com>
2026-06-11 00:41:29 +0800

    Add service worker for PWA caching

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "pwa-test-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-180.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
